6f12aeec8057b9b3ae5dd3fba0ae7a639e0fb2cc
cord <cordc>
2021-06-17 14:59:47 +0200

    17.06.2021 14:59 --REST API working

--- src/main/java/organizer/REST/services/HelloResource.java
+++ src/main/java/organizer/REST/services/HelloResource.java
@@ -3,20 +3,37 @@ package organizer.REST.services;
 import organizer.user.daos.UserDAO;
 import organizer.user.dtos.UserDTO;
 
+import javax.annotation.PostConstruct;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
+
 
 @Path("/bonjour")
-public class HelloResource {
+@Named("rest")
+@SessionScoped
+public class HelloResource implements Serializable {
+boolean loggedIn;
+
+
+
+    @PostConstruct
+    public void init(){
+        this.loggedIn = true;
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String direBonjour() {
-        System.out.println("called");
+if(loggedIn == false){
+    return "Access Denied";
+}
         UserDAO dao = new UserDAO();
         UserDTO users = dao.selectById(1);
         return users.getFirstname() + " " + users.getSurname();
